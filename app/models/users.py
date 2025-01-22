b014e1376c00d5c9a517092a0268106b81141260
blackprince001 <appiahboaduprince@gmail.com>
2025-01-22 12:42:17 +0000

    change id type from integer to uuid

--- app/models/users.py
+++ app/models/users.py
@@ -1,17 +1,25 @@
 from uuid import UUID, uuid4
 
-from sqlalchemy import Column, DateTime, String
+from sqlalchemy import UUID as UUIDType
+from sqlalchemy import DateTime, String
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import func
-
-from app.core.database import Base
+from src.app.db.base_class import Base
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String, unique=True, index=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    # add a role field for doing admin activities
+    id: Mapped[UUID] = mapped_column(UUIDType, primary_key=True, default=uuid4)
+    name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False, index=True)
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+
+    # role: Mapped[UserRole] = mapped_column(
+    #     Enum(UserRole, values_callable=lambda roles: [x.value for x in roles]),
+    #     unique=False,
+    #     default=UserRole.AGGREGATOR,
+    # )
