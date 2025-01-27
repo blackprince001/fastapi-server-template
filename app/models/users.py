ad78c82b49cc25d81b90da1ed4864ecc93e6fc35
blackprince001 <appiahboaduprince@gmail.com>
2025-01-27 15:17:07 +0000

    add verification fields and sign in field

--- app/models/users.py
+++ app/models/users.py
@@ -14,10 +14,18 @@ class User(Base):
     id: Mapped[UUID] = mapped_column(UUIDType, primary_key=True, default=uuid4)
     name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String, unique=True, index=True)
-    hashed_password: Mapped[str] = mapped_column(String, nullable=False, index=True)
+
     created_at: Mapped[DateTime] = mapped_column(
         DateTime(timezone=True), server_default=func.now()
     )
+
+    # security
+    verification_code: Mapped[str] = mapped_column(String, nullable=True)
+    verification_code_expires_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True), nullable=True
+    )
+
+    sign_on_code: Mapped[str] = mapped_column(String, nullable=True)
 
     # role: Mapped[UserRole] = mapped_column(
     #     Enum(UserRole, values_callable=lambda roles: [x.value for x in roles]),
