811e3768ff3c0f6e93cb36e9dfb0693b2627a0a7
blackprince001 <appiahboaduprince@gmail.com>
2025-01-27 15:16:45 +0000

    add create and modify crud to handle user creation,  verification, and login

--- app/crud/users.py
+++ app/crud/users.py
@@ -1,18 +1,27 @@
+from datetime import datetime, timedelta, timezone
 from typing import List
 from uuid import UUID
 
 from sqlalchemy.orm import Session
 
-from app.core.security import get_hash
+from app.core.security import (
+    generate_verification_code,
+    is_signon_code_valid,
+    is_verification_code_valid,
+)
 from app.models.users import User
 from app.schemas.users import UserCreate
 
 
 def create_user(db: Session, user: UserCreate) -> User:
-    user.password = get_hash(user.password)
-    db_user = User(**user.model_dump(exclude="password"))
+    db_user = User(**user.model_dump())
 
-    db_user.hashed_password = user.password
+    verification_code = generate_verification_code()
+
+    db_user.verification_code = verification_code
+    db_user.verification_code_expires_at = datetime.now(timezone.utc) + timedelta(
+        minutes=10
+    )
 
     db.add(db_user)
     db.commit()
@@ -31,3 +40,39 @@ def get_user_by_email(db: Session, email: str) -> User:
 
 def get_users(db: Session, skip: int = 0, limit: int = 100) -> List[User]:
     return db.query(User).offset(skip).limit(limit).all()
+
+
+def verify_user(db: Session, email: str, verification_code: str) -> bool:
+    user = get_user_by_email(db, email)
+    if not user:
+        return False
+
+    if is_verification_code_valid(
+        verification_code, user.verification_code, user.verification_code_expires_at
+    ):
+        user.verification_code = None
+        user.verification_code_expires_at = None
+        db.commit()
+        return True
+
+    return False
+
+
+def verify_login(db: Session, email: str, sign_on_code: str) -> bool:
+    user = get_user_by_email(db, email)
+
+    if not user:
+        return False
+
+    if is_signon_code_valid(sign_on_code, user.sign_on_code):
+        user.sign_on_code = None
+        db.commit()
+
+        return True
+
+    return False
+
+
+def create_signin_code(db: Session, user: User):
+    user.sign_on_code = generate_verification_code()
+    db.commit
