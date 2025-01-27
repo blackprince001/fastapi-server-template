38e84a4f1a2cb2e48cca30017ad4266c9566bdb6
blackprince001 <appiahboaduprince@gmail.com>
2025-01-27 15:15:45 +0000

    passwordless authentication

--- app/core/security.py
+++ app/core/security.py
@@ -1,11 +1,21 @@
-from passlib.context import CryptContext
-
-context = CryptContext(schemes=["argon2"])
-
-
-def get_hash(string: str) -> str:
-    return context.hash(string)
+import random
+from datetime import datetime, timezone
 
 
-def string_matches_hashed(plain: str, hashed: str) -> bool:
-    return context.verify(secret=plain, hash=hashed)
+def generate_verification_code() -> str:
+    return str(random.randint(100000, 999999))
+
+
+def is_verification_code_valid(
+    verification_code: str, stored_code: str, expires_at: datetime
+) -> bool:
+    if not verification_code or not stored_code or not expires_at:
+        return False
+    current_time = datetime.now(timezone.utc)
+    return verification_code == stored_code and current_time < expires_at
+
+
+def is_signon_code_valid(sign_on_code: str, stored_code: str) -> bool:
+    if not sign_on_code or not stored_code:
+        return False
+    return sign_on_code == stored_code
